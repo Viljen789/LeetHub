07812356f6f9aa296a0212dd722c0278a257f6f2
Viljen789 <90694640+Viljen789@users.noreply.github.com>
2024-10-13 16:34:48 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,31 @@
+#define ll long long
+//7 c 3 =8*7*6/3! / 
+//3 c 2 = 4*3*2/2/2
+ll fact(ll a){
+    if(a < 2)return 1;
+    return a*fact(a-1);
+}
+
+
+ll max(ll a, ll b){
+    return a>b?a:b;
+}
+ll min(ll a, ll b){
+    return a>b?b:a;
+}
+
+ll comb(ll a, ll b){
+    //(n+m-1)c(b-1)
+    ll tot = 1;
+    for(int i = 0; i<b-1; i++)tot*=(a+b-2-i);
+    return tot/fact(b-1);
+}
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int>(m));// v[a][b], a er row, b er col
-        for(int i = 0; i<n; i++)dp[i][0] = 1;
-        for(int i = 0; i<m; i++)dp[0][i] = 1;
-        for(int i = 1; i<n; i++){
-            for(int j = 1; j<m; j++)dp[i][j] = dp[i-1][j]+ dp[i][j-1];
-        }
-        return dp[n-1][m-1];
+        if(min(n, m)<=1)return(1);
+        if(min(n, m)<=2)return(max(n, m));
+        return comb(max(m, n), min(m, n));
     }
 };
