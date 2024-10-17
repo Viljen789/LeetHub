984bfaa2abf51c54d746d4b0ffad3044a321ed02
Viljen789 <90694640+Viljen789@users.noreply.github.com>
2024-10-17 14:50:35 +0200

    Time: 11 ms (99.73%), Space: 104 MB (5.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,16 @@
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int curmin = 1e9, curmax = -1e9, curtot = 0;
-        curmin = min(curmin, prices[0]);
-        for (int i = 1; i < prices.size()-1; i++) {
-            if (prices[i] >= prices[i - 1] && prices[i] > prices[i + 1]) {
-                curmax = max(prices[i], curmax);
+        int cm = 0;
+        int n = prices.size();
+        vector<int> sell(n);
+        vector<int> prof(n, 0);
+        sell[n-1] = prices[n-1];
+        for(int i = n-2; i>=0; i--)sell[i] =max(sell[i+1], prices[i]);
+        prof[0] = sell[0]-prices[0];
+        for(int i = 1; i<n; i++)prof[i] = max(prof[i-1], sell[i]-prices[i]);
+        return prof[n-1];
 
-            }
-            if (prices[i] <= prices[i - 1] && prices[i] < prices[i + 1]){
-                if(prices[i]<curmin){
-                    curmin = min(prices[i], curmin);
-                    curmax = -1e9;
-                }
-            }
-               
-            curtot = max(curtot, curmax - curmin);
-        }
-        curmax = max(curmax, prices[prices.size()-1]);
-        curtot = max(curtot, curmax - curmin);
-        return curtot;
     }
 };
